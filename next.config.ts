950f5dffea41643a381cd76816fd5d809f84636f
girquirrel <elisabetaa.russo.02@gmail.com>
2026-09-16 23:30:09 +0200

    changes

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     DATABASE_URL: process.env.DATABASE_URL,
     DIRECT_URL: process.env.DIRECT_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_REGION: process.env.S3_REGION,
+    S3_UPLOAD_BUCKET: process.env.S3_UPLOAD_BUCKET,
   },
   experimental: {
     serverActions: {
